6f159091348a09608c5121721c5d885215472132
denarced <denarced@gmail.com>
2018-07-20 16:11:19 +0300

    Create playlist with only files with specific extensions.

--- vlcall.sh
+++ vlcall.sh
@@ -10,8 +10,63 @@
 # Create playlist to tmp
 playlist=`mktemp --suffix=.m3u`
 # Playlist was created to /tmp because otherwise `find` would find it and add it
-# to playlist. Also Chrome downloads and m3u playlists are ignored.
-find . -type f ! -name '*.crdownload' ! -name '*.m3u' > $playlist
+# to playlist. The video file extensions list is from VLC source code.
+find . -type f \( \
+    -iname \*.3g2 \
+    -o -iname \*.3gp \
+    -o -iname \*.3gp2 \
+    -o -iname \*.3gpp \
+    -o -iname \*.amv \
+    -o -iname \*.asf \
+    -o -iname \*.avi \
+    -o -iname \*.bik \
+    -o -iname \*.divx \
+    -o -iname \*.drc \
+    -o -iname \*.dv \
+    -o -iname \*.f4v \
+    -o -iname \*.flv \
+    -o -iname \*.gvi \
+    -o -iname \*.gxf \
+    -o -iname \*.m1v \
+    -o -iname \*.m2t \
+    -o -iname \*.m2ts \
+    -o -iname \*.m2v \
+    -o -iname \*.m4v \
+    -o -iname \*.mkv \
+    -o -iname \*.mov \
+    -o -iname \*.mp2v \
+    -o -iname \*.mp4 \
+    -o -iname \*.mp4v \
+    -o -iname \*.mpa \
+    -o -iname \*.mpe \
+    -o -iname \*.mpeg \
+    -o -iname \*.mpeg1 \
+    -o -iname \*.mpeg2 \
+    -o -iname \*.mpeg4 \
+    -o -iname \*.mpg \
+    -o -iname \*.mpv2 \
+    -o -iname \*.mts \
+    -o -iname \*.mtv \
+    -o -iname \*.mxf \
+    -o -iname \*.nsv \
+    -o -iname \*.nuv \
+    -o -iname \*.ogg \
+    -o -iname \*.ogm \
+    -o -iname \*.ogv \
+    -o -iname \*.ogx \
+    -o -iname \*.rec \
+    -o -iname \*.rm \
+    -o -iname \*.rmvb \
+    -o -iname \*.rpl \
+    -o -iname \*.thp \
+    -o -iname \*.tod \
+    -o -iname \*.ts \
+    -o -iname \*.tts \
+    -o -iname \*.vob \
+    -o -iname \*.vro \
+    -o -iname \*.webm \
+    -o -iname \*.wmv \
+    -o -iname \*.xesc \) > $playlist
 
 # Randomize playlist if requested
 # The parentheses are not strictly speaking necessary
